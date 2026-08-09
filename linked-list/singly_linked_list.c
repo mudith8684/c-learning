f98b62caa65d2f7c631c147b009c76406c02bfcf
mudith8684 <mudith2007@gmail.com>
2026-08-09 20:01:36 +0530

    added insertion of nodes, testing

--- linked-list/singly_linked_list.c
+++ linked-list/singly_linked_list.c
@@ -59,6 +59,47 @@ void search_sll (struct Node *head, int key) {
     }
 }
 
+// Inserting a node
+struct Node* insert_beginning (struct Node *head, int data) {
+    struct Node *temp = createNode(data);
+    temp->next = head;
+    head = temp;
+    return head;
+}
+
+struct Node* insert_end (struct Node *head, int data) {
+    struct Node *temp = createNode(data);
+    if (head == NULL) {
+        return temp;
+    }
+    struct Node *cur = head;
+    while (cur != NULL) {
+        cur = cur->next;
+    }
+    cur->next = temp;
+    return head;
+}
+
+struct Node* insert_position(struct Node* head, int data, int pos) {
+    struct Node* temp = createNode(data);
+    if (pos == 0) {
+        temp->next = head;
+        return temp;
+    }
+    struct Node* cur = head;
+    for (int i = 0; i < pos - 1 && cur != NULL; i++) {
+        cur = cur->next;
+    }
+    if (cur == NULL) {
+        free(temp);
+        printf("Position out of bounds.\n");
+        return head;
+    }
+    temp->next = cur->next;
+    cur->next = temp;
+    return head;
+}
+
 int main () {
     printf("Singly Linked List\n");
     struct Node *head = NULL;
@@ -79,6 +120,12 @@ int main () {
     }
     printf("The sum of the elements is: %d\n", sum);
     // freeing entire list using temp
+    head = insert_beginning(head, 5);
+    traversal_sll(head);
+    head = insert_end(head, 50);
+    traversal_sll(head);
+    head = insert_position(head, 15, 3);
+    traversal(head);
     temp = head;
     while (temp != NULL) {
         struct Node *next = temp->next;
